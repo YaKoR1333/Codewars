8a01ac0d99d6da128a3348fb91c692cf3efd550d
Daniil_Rodionov <103533107+YaKoR1333@users.noreply.github.com>
2022-05-27 00:11:56 +0300

    Create Sum of Intervals.py

---
+++ 4-kyu/Sum of Intervals.py
@@ -0,0 +1,15 @@
+def sum_of_intervals(intervals):
+    number = []
+    maxn = 0
+    minn = 0
+    for i in intervals:
+        if i[1] > maxn:
+            maxn = i[1]
+        if i[0] < minn:
+            minn = i[0]
+    for i in range(0, maxn - minn):
+        number.append(0)
+    for i in intervals:
+        for j in range(i[0]-minn, i[1]-minn):
+            number[j] = 1   
+    return sum(number)
